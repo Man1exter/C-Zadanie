532908d3aad7f558c7fc881df05de4b2e0fb8126
Mariusz Perzynski <m.perzyk@interia.pl>
2023-10-08 13:19:41 +0200

    add menu

--- EFCoreExample/Program.cs
+++ EFCoreExample/Program.cs
@@ -10,28 +10,74 @@ namespace EFCoreExample
         {
             using (var context = new BlogContext())
             {
-                // Create - Dodawanie nowego bloga
-                var newBlog = new Blog { Name = "Moje blogowanie Mariusz Perzynski" };
-                context.Blogs.Add(newBlog);
-                context.SaveChanges();
-
-                // Read - Wyświetlanie wszystkich blogów
-                var blogs = context.Blogs.ToList();
-                foreach (var blog in blogs)
+                while (true)
                 {
-                    Console.WriteLine($"BlogId: {blog.BlogId}, Name: {blog.Name}");
+                    Console.WriteLine("Wybierz opcję:");
+                    Console.WriteLine("1. Wyświetl wszystkie blogi");
+                    Console.WriteLine("2. Dodaj nowy blog");
+                    Console.WriteLine("3. Edytuj blog");
+                    Console.WriteLine("4. Wyjdź z programu");
+
+                    var choice = Console.ReadLine();
+
+                    switch (choice)
+                    {
+                        case "1":
+                            // Read - Wyświetlanie wszystkich blogów
+                            var blogs = context.Blogs.ToList();
+                            Console.WriteLine("Lista blogów:");
+                            foreach (var blog in blogs)
+                            {
+                                Console.WriteLine($"BlogId: {blog.BlogId}, Name: {blog.Name}");
+                            }
+                            break;
+
+                        case "2":
+                            // Create - Dodawanie nowego bloga
+                            Console.WriteLine("Podaj nazwę nowego bloga:");
+                            var newBlogName = Console.ReadLine();
+                            var newBlog = new Blog { Name = newBlogName };
+                            context.Blogs.Add(newBlog);
+                            context.SaveChanges();
+                            Console.WriteLine("Nowy blog został dodany.");
+                            break;
+
+                        case "3":
+                            // Update - Edytowanie bloga
+                            Console.WriteLine("Podaj BlogId bloga do edycji:");
+                            if (int.TryParse(Console.ReadLine(), out var blogIdToUpdate))
+                            {
+                                var blogToUpdate = context.Blogs.FirstOrDefault(b => b.BlogId == blogIdToUpdate);
+                                if (blogToUpdate != null)
+                                {
+                                    Console.WriteLine("Podaj nową nazwę bloga:");
+                                    var newBlogNameToUpdate = Console.ReadLine();
+                                    blogToUpdate.Name = newBlogNameToUpdate;
+                                    context.SaveChanges();
+                                    Console.WriteLine("Nazwa bloga została zaktualizowana.");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Blog o podanym BlogId nie istnieje.");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Podano nieprawidłowy numer BlogId.");
+                            }
+                            break;
+
+                        case "4":
+                            // Wyjście z programu
+                            return;
+
+                        default:
+                            Console.WriteLine("Nieprawidłowy wybór. Wybierz opcję od 1 do 4.");
+                            break;
+                    }
                 }
-
-                // Update - Aktualizacja nazwy bloga
-                var blogToUpdate = context.Blogs.First();
-                blogToUpdate.Name = "Nowa nazwa bloga";
-                context.SaveChanges();
-
-                // Delete - Usunięcie bloga
-                var blogToDelete = context.Blogs.First(b => b.Name == "Nowa nazwa bloga");
-                context.Blogs.Remove(blogToDelete);
-                context.SaveChanges();
             }
         }
     }
 }
+
